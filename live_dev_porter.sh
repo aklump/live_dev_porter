a0fc219d8f9cb45e8fbdbab79f501fbedb928144
Aaron Klump <sourcecode@intheloftstudios.com>
2021-09-10 20:28:46 -0700

    Add config route.

--- live_dev_porter.sh
+++ live_dev_porter.sh
@@ -91,6 +91,17 @@ fi
 command=$(get_command)
 case $command in
 
+    "config")
+      if [[ ! "$EDITOR" ]]; then
+        exit_with_failure "You must set environment variable \$EDITOR with a command first, e.g. in ~/.bash_profile, export EDITOR=nano"
+      fi
+      config_file="$CONFIG_DIR/config.yml"
+      if has_option 'local'; then
+        config_file="$CONFIG_DIR/config.local.yml"
+      fi
+      $EDITOR $config_file && exit_with_cache_clear
+      ;;
+
     "init")
       source "$SOURCE_DIR/init.sh"
       for plugin in "${ACTIVE_PLUGINS[@]}"; do
