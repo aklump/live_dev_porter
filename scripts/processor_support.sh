4e9597dcac1b1dddadbbb068383f036d54c8e18b
Aaron Klump <sourcecode@intheloftstudios.com>
2022-07-21 21:51:23 -0700

    Fix bug with query()

--- scripts/processor_support.sh
+++ scripts/processor_support.sh
@@ -17,9 +17,9 @@ function query() {
   local query="$1"
 
   local feedback
-  eval $(get_config_as "write_access" "environments.$ENVIRONMENT_ID.write_access" false)
+  eval $(get_config_as "write_access" "environments.$LOCAL_ENV_ID.write_access" false)
   if [[ "$write_access" != true ]]; then
-    feedback="query() can only be used if the environment \"$ENVIRONMENT_ID\" has write_access, you must change your configuration to allow this."
+    feedback="query() can only be used if the environment \"$LOCAL_ENV_ID\" has write_access, you must change your configuration to allow this."
     write_log_error "$feedback"
     throw "$feedback;$0;in function ${FUNCNAME}();$LINENO"
   fi
@@ -27,8 +27,8 @@ function query() {
   local defaults_file
   local db_name
   query_result="$CACHE_DIR/database_result.sql"
-  defaults_file=$(database_get_defaults_file "$ENVIRONMENT_ID" "$DATABASE_ID")
-  db_name=$(database_get_name "$ENVIRONMENT_ID" "$DATABASE_ID") || return 1
+  defaults_file=$(database_get_defaults_file "$LOCAL_ENV_ID" "$DATABASE_ID")
+  db_name=$(database_get_name "$LOCAL_ENV_ID" "$DATABASE_ID") || return 1
   ! mysql --defaults-file="$defaults_file" "$db_name" -e "$query" > "$query_result" && "Failed query: $query" && return 1
   # This removes the header row
   tail -n +2 "$query_result" > "$query_result.tmp" && mv "$query_result.tmp" "$query_result"
