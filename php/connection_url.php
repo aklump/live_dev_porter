a55314ee5a5b6e45a9ebde035694896931679edd
Aaron Klump <sourcecode@intheloftstudios.com>
2022-10-27 21:33:54 -0700

    Blackout the PW in the connection string.

--- php/connection_url.php
+++ php/connection_url.php
@@ -1,4 +1,9 @@
 <?php
+/**
+ * @file
+ *
+ * Convert a .cnf filepath to a connection URL.
+ */
 
 $cnf = file_get_contents($argv[1]);
 preg_match_all("/^(.+)=(.+)$/m", $cnf, $matches);
@@ -6,7 +11,7 @@ $values = array_map(function ($value) {
   return trim($value, '"');
 }, $matches[2]);
 $creds = array_combine($matches[1], $values);
-$url = sprintf('mysql://%s:%s:%s', $creds['user'], $creds['password'], $creds['host']);
+$url = sprintf('mysql://%s:%s:%s', $creds['user'], 'PASSWORD', $creds['host']);
 if (!empty($creds['port'])) {
   $url .= ':' . $creds['port'];
 }
