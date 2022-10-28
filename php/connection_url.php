05643372db049c94d9127a1f24c3529d3b7158e8
Aaron Klump <sourcecode@intheloftstudios.com>
2022-10-27 21:39:39 -0700

    Fix @ in url.

--- php/connection_url.php
+++ php/connection_url.php
@@ -11,7 +11,7 @@ $values = array_map(function ($value) {
   return trim($value, '"');
 }, $matches[2]);
 $creds = array_combine($matches[1], $values);
-$url = sprintf('mysql://%s:%s:%s', $creds['user'], 'PASSWORD', $creds['host']);
+$url = sprintf('mysql://%s:%s@%s', $creds['user'], 'PASSWORD', $creds['host']);
 if (!empty($creds['port'])) {
   $url .= ':' . $creds['port'];
 }
