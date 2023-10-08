a508c4dae7af36355936dc03f952decc8b96446a
Aaron Klump <sourcecode@intheloftstudios.com>
2023-10-08 12:21:56 -0700

    Add PHP 8 compatibility.

--- cloudy/version.sh
+++ cloudy/version.sh
@@ -1,3 +1,3 @@
-cloudy_update__last_update=1695780442
-cloudy_update__version="1.5.9"
+cloudy_update__last_update=1696797075
+cloudy_update__version="1.5.11"
 cloudy_update__source="https://github.com/aklump/cloudy"
