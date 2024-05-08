972806c87e6b32af4cb13a70ad94dd05b9e7d2ef
Aaron Klump <sourcecode@intheloftstudios.com>
2024-05-08 10:13:37 -0700

    Update Cloudy

--- cloudy/inc/cloudy.core.sh
+++ cloudy/inc/cloudy.core.sh
@@ -99,7 +99,7 @@ if [[ ! -f "$CACHED_CONFIG_FILEPATH" ]]; then
   [[ "$cloudy_development_skip_config_validation" == true ]] && write_log_dev_warning "Configuration validation is disabled due to \$cloudy_development_skip_config_validation == true."
 
   # Convert the JSON to bash config.
-  php "$CLOUDY_ROOT/php/json_to_bash.php" "$ROOT" "cloudy_config" "$CLOUDY_CONFIG_JSON" >"$CACHED_CONFIG_FILEPATH"
+  "$CLOUDY_PHP" "$CLOUDY_ROOT/php/json_to_bash.php" "$ROOT" "cloudy_config" "$CLOUDY_CONFIG_JSON" >"$CACHED_CONFIG_FILEPATH"
   json_to_bash_result=$?
   if [[ $json_to_bash_result -ne 0 ]]; then
     fail_because "$(cat "$CACHED_CONFIG_FILEPATH"|tr -d '\n')"
--- cloudy/version.sh
+++ cloudy/version.sh
@@ -1,3 +1,3 @@
-cloudy_update__last_update=1715133776
-cloudy_update__version="1.7.0"
+cloudy_update__last_update=1715188405
+cloudy_update__version="1.7.1"
 cloudy_update__source="https://github.com/aklump/cloudy"
