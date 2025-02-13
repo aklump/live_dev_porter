c99ff56b262a89a54ea8b09a0372225047bbadd7
Aaron Klump <sourcecode@intheloftstudios.com>
2025-02-13 10:52:21 -0800

    Add perms.sh script to set execute permissions
    This script ensures the necessary files have execute permissions for proper functionality.

---
+++ bin/perms.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+s="${BASH_SOURCE[0]}";[[ "$s" ]] || s="${(%):-%N}";while [ -h "$s" ];do d="$(cd -P "$(dirname "$s")" && pwd)";s="$(readlink "$s")";[[ $s != /* ]] && s="$d/$s";done;__DIR__=$(cd -P "$(dirname "$s")" && pwd)
+
+chmod u+x $__DIR__/ldp
+chmod u+x $__DIR__/../live_dev_porter.sh
